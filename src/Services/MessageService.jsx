a78a9dff93467cc9b81f2964e1fc383c691acca6
Tarun Singh LNMIIT <thetseffect@gmail.com>
2020-06-23 16:44:43 +0530

    Added PROXY

--- src/Services/MessageService.jsx
+++ src/Services/MessageService.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 export default {
   sendMessage: (message) => {
     return axios
-      .post("/api/message/newmessage", { message })
+      .post(
+        "https://portfolio-server-mernapp.herokuapp.com/api/message/newmessage",
+        { message }
+      )
       .then((res) => {
         if (res.status == 200) {
           console.log("RES : ", res);
